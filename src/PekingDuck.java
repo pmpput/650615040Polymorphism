f93fce9e2808b677675fe10e72a581a4969ea088
pmpput <118797624+pmpput@users.noreply.github.com>
2023-07-23 11:38:34 +0700

    PekingDuck.java

--- src/PekingDuck.java
+++ src/PekingDuck.java
@@ -2,7 +2,7 @@ public class PekingDuck extends Duck{
     
     @Override
     public void clean(Animal prey) {
-        System.out.println(prey + "I cannot clane. I died already.");
+        System.out.println(prey + "I cannot cleane. I died already.");
     }
     
 }
